be8b92250f08b09308c3040f3f7334fa49f16a98
TGX03 <tgx003@gmail.com>
2026-01-20 20:40:18 +0100

    Create ability to modify zonefile with prefix modification

---
+++ src/eu/tgx03/ddns/IPv6Prefix.java
@@ -0,0 +1,223 @@
+package eu.tgx03.ddns;
+
+import java.net.Inet6Address;
+import java.net.UnknownHostException;
+
+/**
+ * Represents an IPv6 network prefix by storing its upper and lower 64 bits and
+ * the corresponding suffix masks used to apply the prefix to IP addresses.
+ * This class provides utilities for working with IPv6 prefixes and addresses,
+ * including conversion between different networks.
+ */
+public class IPv6Prefix {
+
+    /**
+     * The upper 64 bits of this prefix.
+     */
+    private final long upper;
+    /**
+     * The lower 64 bits of this prefix.
+     */
+    private final long lower;
+
+    /**
+     * The upper 64 bits of the suffix mask used to adjust IP addresses to this prefix.
+     */
+    private final long upper_suffix_mask;
+    /**
+     * The lower 64 bits of the suffix mask used to adjust IP addresses to this prefix.
+     */
+    private final long lower_suffix_mask;
+
+    /**
+     * Constructs an IPv6Prefix object by converting the given IPv6 address into its
+     * upper and lower 64-bit components and zeroing the remaining bits.
+     *
+     * @param prefixLength the prefix length, representing the number of bits in the address
+     *                     that constitute the network portion. Must be in the range 0 to 128.
+     * @param address      the IPv6 address represented as an {@link Inet6Address} object.
+     *                     This address is used to determine the associated upper and lower
+     *                     components for the prefix.
+     * @throws IllegalArgumentException if the prefix length is not in the valid range
+     *                                  [0, 128].
+     */
+    public IPv6Prefix(int prefixLength, Inet6Address address) {
+        IPv6Long prefix = IPv6ToLong(address);
+        IPv6Long mask = prefixMask(prefixLength);
+        this.upper = prefix.upper & mask.upper;
+        this.lower = prefix.lower & mask.lower;
+
+        IPv6Long suffixMask = suffixMask(prefixLength);
+        this.upper_suffix_mask = suffixMask.upper;
+        this.lower_suffix_mask = suffixMask.lower;
+    }
+
+    /**
+     * Converts an IPv6 address represented as an {@link Inet6Address} object into its 128-bit
+     * representation, separated into two 64-bit parts, upper and lower.
+     *
+     * @param address the IPv6 address to be converted, provided as an {@link Inet6Address} object.
+     *                This address is broken down into its high-order 64 bits and low-order 64 bits.
+     * @return an {@link IPv6Long} object containing the upper and lower 64-bit components
+     *         of the given IPv6 address.
+     */
+    public static IPv6Long IPv6ToLong(Inet6Address address) {
+        byte[] bytes = address.getAddress();
+        long upper = Byte.toUnsignedLong(bytes[0]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[1]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[2]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[3]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[4]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[5]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[6]);
+        upper <<= 8;
+        upper |= Byte.toUnsignedLong(bytes[7]);
+        long lower = Byte.toUnsignedLong(bytes[8]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[9]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[10]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[11]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[12]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[13]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[14]);
+        lower <<= 8;
+        lower |= Byte.toUnsignedLong(bytes[15]);
+        return new IPv6Long(upper, lower);
+    }
+
+    /**
+     * Converts a 128-bit IPv6 address, represented as an {@link IPv6Long} object,
+     * into an {@link Inet6Address} object.
+     *
+     * @param address the input IPv6 address represented as an {@link IPv6Long} object,
+     *                containing the high-order 64 bits (upper) and low-order 64 bits (lower)
+     *                of the address.
+     * @return the {@link Inet6Address} object corresponding to the provided IPv6 address.
+     * @throws RuntimeException if the provided address cannot be converted into an {@link Inet6Address},
+     *                          typically caused by an {@link UnknownHostException}.
+     */
+    public static Inet6Address longToInet6Address(IPv6Long address) {
+        byte[] bytes = new byte[16];
+        bytes[0] = (byte) (address.upper >> 56);
+        bytes[1] = (byte) (address.upper >> 48);
+        bytes[2] = (byte) (address.upper >> 40);
+        bytes[3] = (byte) (address.upper >> 32);
+        bytes[4] = (byte) (address.upper >> 24);
+        bytes[5] = (byte) (address.upper >> 16);
+        bytes[6] = (byte) (address.upper >> 8);
+        bytes[7] = (byte) (address.upper);
+        bytes[8] = (byte) (address.lower >> 56);
+        bytes[9] = (byte) (address.lower >> 48);
+        bytes[10] = (byte) (address.lower >> 40);
+        bytes[11] = (byte) (address.lower >> 32);
+        bytes[12] = (byte) (address.lower >> 24);
+        bytes[13] = (byte) (address.lower >> 16);
+        bytes[14] = (byte) (address.lower >> 8);
+        bytes[15] = (byte) (address.lower);
+        try {
+            return (Inet6Address) Inet6Address.getByAddress(bytes);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Generates a prefix mask for an IPv6 address based on the specified prefix length.
+     * The mask is represented as an {@link IPv6Long} object with two 64-bit components
+     * (upper and lower) defining the network bits of the IPv6 prefix.
+     *
+     * @param prefixLength the length of the network prefix, ranging from 0 to 128. A
+     *                     value of 0 provides a mask of all zeroes, while a value of
+     *                     128 provides a mask of all ones.
+     * @return an {@link IPv6Long} object representing the prefix mask, where the
+     *         network bits are set to 1 up to the specified prefix length, and the
+     *         remaining bits are set to 0.
+     * @throws IllegalArgumentException if the prefix length is outside the valid range [0, 128].
+     */
+    private static IPv6Long prefixMask(int prefixLength) {
+        if (prefixLength == 0) return new IPv6Long(0L, 0L);
+        if (prefixLength < 0 || prefixLength > 128) throw new IllegalArgumentException("Invalid prefix length: " + prefixLength);
+
+        if (prefixLength == 64) {
+            return new IPv6Long(-1L, 0L);
+        }
+
+        else if (prefixLength == 128) {
+            return new IPv6Long(-1L, -1L);
+        }
+
+        else if (prefixLength < 64) {
+            long upper = -1L << (64 - prefixLength);
+            return new IPv6Long(upper, 0L);
+        }
+
+        else {
+            long lower = -1L << (128 - prefixLength);
+            return new IPv6Long(-1L, lower);
+        }
+    }
+
+    /**
+     * Generates a suffix mask for an IPv6 address based on the specified prefix length.
+     * The suffix mask is represented as an {@link IPv6Long} object, with the non-network bits
+     * set to 1 and the network bits set to 0, starting at the specified prefix length.
+     *
+     * @param prefixLength the length of the network prefix, ranging from 0 to 128. A value of 0
+     *                     results in a mask with all bits set to 1, and a value of 128 results
+     *                     in a mask with all bits set to 0.
+     * @return an {@link IPv6Long} object representing the suffix mask, where bits outside the
+     *         network portion are set to 1 and network bits are set to 0.
+     * @throws IllegalArgumentException if the prefix length is outside the valid range [0, 128].
+     */
+    private static IPv6Long suffixMask(int prefixLength) {
+        if (prefixLength == 0) return new IPv6Long(-1L, -1L);
+        if (prefixLength < 0 || prefixLength > 128) throw new IllegalArgumentException("Invalid prefix length: " + prefixLength);
+
+        if (prefixLength == 64) {
+            return new IPv6Long(0L, -1L);
+        }
+
+        else if (prefixLength == 128) {
+            return new IPv6Long(0L, 0L);
+        }
+
+        else if (prefixLength <= 64) {
+            long upper = -1L >>> prefixLength;
+            return new IPv6Long(upper, -1L);
+        }
+
+        else {
+            long lower = -1 >>> (128 - prefixLength);
+            return new IPv6Long(0L, lower);
+        }
+    }
+
+    /**
+     * Changes the prefix of the given IPv6 address to this prefix.
+     * @param address The address to modify.
+     * @return The address with the new prefix.
+     */
+    public Inet6Address adjustPrefix(Inet6Address address) {
+        IPv6Long addressLong = IPv6ToLong(address);
+        long upperSuffix = addressLong.upper & this.upper_suffix_mask;
+        long lowerSuffix = addressLong.lower & this.lower_suffix_mask;
+
+        long upperResult = this.upper |  upperSuffix;
+        long lowerResult = this.lower | lowerSuffix;
+
+        return longToInet6Address(new IPv6Long(upperResult, lowerResult));
+    }
+
+    public record IPv6Long(long upper, long lower) {}
+}
---
+++ src/eu/tgx03/ddns/Zonefile.java
@@ -0,0 +1,217 @@
+package eu.tgx03.ddns;
+
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * The Zonefile class parses a zonefile,
+ * receives IPv4 addresses, IPv6 addresses, and IPv6 prefixes,
+ * and, upon invocation of getZoneFile adjusts the previously loaded zonefile
+ * so the placeholders are replaced, thereby especially making sure IPv6 addresses have their prefixes adjusted.
+ */
+public class Zonefile {
+
+    /**
+     * The regex not actually being used to identify IPv4 addresses to be replaced.
+     */
+    private static final String IPV4_REGEX = "IP4_(\\d+)";
+    /**
+     * The regex not actually being used to identify IPv6 addresses to be replaced.
+     */
+    private static final String IPV6_REGEX = "IP6_(\\d+)";
+    /**
+     * A regular expression pattern that matches IPv6 suffix definitions in zone files.
+     * The pattern is designed to parse strings representing mappings of an index to
+     * an IPv6 suffix in the format "IP6_P_<index>=<IPv6-suffix>".
+     *
+     * - The first capturing group (\d+) captures the numeric index.
+     * - The second capturing group ([0-9a-f:]{2,39}) captures the IPv6 suffix,
+     *   which is represented in hexadecimal colon-separated notation.
+     */
+    private static final String IPv6_PREFIX_REGEX = "IP6_P_(\\d+)=([0-9a-f:]{2,39})";
+
+    /**
+     * The original zone file as it was loaded into the program.
+     */
+    private final String zoneBase;
+    private final Lock ipv4ReadLock;
+    private final Lock ipv4WriteLock;
+    private final Lock ipv6ReadLock;
+    private final Lock ipv6WriteLock;
+    private final Lock ipv6PrefixReadLock;
+    private final Lock ipv6PrefixWriteLock;
+
+    /**
+     * An array holding all the IPv4 addresses to be put into the zonefile later on.
+     */
+    private Inet4Address[] ipv4Addresses = new Inet4Address[1];
+    /**
+     * An array holding all the IPv6 addresses to be put into the zonefile later on.
+     */
+    private Inet6Address[] ipv6Addresses = new Inet6Address[0];
+    /**
+     * An array holding all the IPv6 prefixes to be merged with the suffixes in the zone file.
+     */
+    private IPv6Prefix[] prefixes = new IPv6Prefix[1];
+
+    {
+        ReentrantReadWriteLock ipv4Lock = new ReentrantReadWriteLock();
+        this.ipv4ReadLock = ipv4Lock.readLock();
+        this.ipv4WriteLock = ipv4Lock.writeLock();
+        ReentrantReadWriteLock ipv6Lock = new ReentrantReadWriteLock();
+        this.ipv6ReadLock = ipv6Lock.readLock();
+        this.ipv6WriteLock = ipv6Lock.writeLock();
+        ReentrantReadWriteLock ipv6PrefixLock = new ReentrantReadWriteLock();
+        this.ipv6PrefixReadLock = ipv6PrefixLock.readLock();
+        this.ipv6PrefixWriteLock = ipv6PrefixLock.writeLock();
+    }
+
+    /**
+     * Creates a new instance of Zonefile from the provided String.
+     * The String contains the plain zonefile data and replace operations will be executed directly upon it.
+     * @param zoneBase The String of the zonefile
+     */
+    public Zonefile(String zoneBase) {
+        this.zoneBase = zoneBase;
+    }
+
+    /**
+     * Constructs a new Zonefile instance using the provided InputStream.
+     * The InputStream supplies the zonefile data, which will be processed as a single string.
+     *
+     * @param input The InputStream from which the zonefile data is read.
+     */
+    public Zonefile(InputStream input) {
+        this.zoneBase = new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    /**
+     * Constructs a new Zonefile instance using the provided File.
+     * The File should contain the zonefile data, which is read
+     * and stored as a single string for further processing.
+     *
+     * @param source The File that contains the zonefile data to load.
+     * @throws IOException If an I/O error occurs while reading the file.
+     */
+    public Zonefile(File source) throws IOException {
+        try (FileReader reader = new FileReader(source)) {
+            this.zoneBase = reader.readAllLines().stream().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+
+    /**
+     * Sets the IPv4 address which should be inserted into the zonefile at index {@code index}.
+     *
+     * @param index   The zero-based index at which to set the IPv4 address.
+     * @param address The IPv4 address to set at the specified index.
+     */
+    public void setIpv4Address(int index, Inet4Address address) {
+        if (index >= this.ipv4Addresses.length) {
+            ipv4WriteLock.lock();
+            if (index >= this.ipv4Addresses.length) this.ipv4Addresses = Arrays.copyOf(this.ipv4Addresses, index + 1);
+            ipv4WriteLock.unlock();
+        }
+        ipv4ReadLock.lock();
+        this.ipv4Addresses[index] = address;
+        ipv4ReadLock.unlock();
+    }
+
+    /**
+     * Sets the IPv6 address which should be inserted into the zonefile at index {@code index}.
+     *
+     * @param index   The zero-based index at which to set the IPv4 address.
+     * @param address The IPv6 address to set at the specified index.
+     */
+    public void setIpv6Address(int index, Inet6Address address) {
+        if (index >= this.ipv6Addresses.length) {
+            ipv6WriteLock.lock();
+            if (index >= this.ipv6Addresses.length) this.ipv6Addresses = Arrays.copyOf(this.ipv6Addresses, index + 1);
+            ipv6WriteLock.unlock();
+        }
+        ipv6ReadLock.lock();
+        this.ipv6Addresses[index] = address;
+        ipv6ReadLock.unlock();
+    }
+
+    /**
+     * Sets the IPv6 prefix at the specified index in the zonefile.
+     * If the index exceeds the current capacity of the prefix array,
+     * the array is resized to accommodate the specified index.
+     *
+     * @param index  The zero-based index at which to set the IPv6 prefix.
+     * @param prefix The IPv6Prefix object to set at the specified index.
+     */
+    public void setIpv6Prefix(int index, IPv6Prefix prefix) {
+        if (index >= this.prefixes.length) {
+            ipv6PrefixWriteLock.lock();
+            if (index >= this.prefixes.length) this.prefixes = Arrays.copyOf(this.prefixes, index + 1);
+            ipv6PrefixWriteLock.unlock();
+        }
+        ipv6PrefixReadLock.lock();
+        this.prefixes[index] = prefix;
+        ipv6PrefixReadLock.unlock();
+    }
+
+    /**
+     * Generates a zone file with placeholders replaced by the appropriate IPv4 and IPv6
+     * addresses. The method performs the following operations in sequence:
+     * 1. Replace IPv4 placeholders in the format `IP4_{index}` within the zone file
+     *    with the corresponding IPv4 addresses.
+     * 3. Replaces IPv6 placeholders in the format `IP6_{index}` within the zone file
+     *    with the corresponding IPv6 addresses.
+     * 4. Identifies and processes IPv6 prefix placeholders in the zone file, adjusts the
+     *    prefixes accordingly, and replaces them in the zone file.
+     *
+     * @return The updated zone file string after performing all address and prefix replacements.
+     * @throws UnknownHostException If address resolution fails at any point during the operation.
+     */
+    public String getZoneFile() throws UnknownHostException {
+        ipv4WriteLock.lock();
+        ipv6WriteLock.lock();
+        ipv6PrefixWriteLock.lock();
+
+        String result = this.zoneBase;
+
+        for (int i = 0; i < ipv4Addresses.length; i++) {
+            if (this.ipv4Addresses[i] == null) continue;
+            result = result.replaceAll("IP4_" + i, ipv4Addresses[i].getHostAddress());
+        }
+
+        for (int i = 0; i < ipv6Addresses.length; i++) {
+            if (this.ipv6Addresses[i] == null) continue;
+            result = result.replaceAll("IP6_" + i, ipv6Addresses[i].getHostAddress());
+        }
+
+        HashMap<String, Inet6Address> ipv6Prefixes = new HashMap<>();
+        Pattern ipv6PrefixPattern = Pattern.compile(IPv6_PREFIX_REGEX);
+        Matcher matcher = ipv6PrefixPattern.matcher(result);
+        while (matcher.find()) {
+            if (ipv6Prefixes.containsKey(matcher.group())) continue;
+            int prefix = Integer.parseInt(matcher.group(1));
+            Inet6Address address = (Inet6Address) Inet6Address.getByName(matcher.group(2));
+            if (this.prefixes[prefix] != null) {
+                Inet6Address adjustedAddress = this.prefixes[prefix].adjustPrefix(address);
+                ipv6Prefixes.put(matcher.group(), adjustedAddress);
+            }
+        }
+
+        for (Map.Entry<String, Inet6Address> entry : ipv6Prefixes.entrySet()) {
+            result = result.replaceAll(entry.getKey(), entry.getValue().getHostAddress());
+        }
+
+        ipv4WriteLock.unlock();
+        ipv6WriteLock.unlock();
+        ipv6PrefixWriteLock.unlock();
+        return result;
+    }
+}
